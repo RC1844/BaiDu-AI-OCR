2cfbc742c6ba32426d76668e5c055d5056a4a136
1844766234 <1844766234@qq.com>
2019-01-03 15:49:31 +0800

    1.7.3修正版本号

--- BaiDu-AI/Properties/AssemblyInfo.cs
+++ BaiDu-AI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("BaiDu-AI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("文字识别")]
+[assembly: AssemblyDescription("基于百度AI的文字识别工具")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaiDu-AI")]
+[assembly: AssemblyCompany("个人")]
+[assembly: AssemblyProduct("文字识别")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.3.0")]
